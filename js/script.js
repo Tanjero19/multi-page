27aaa8b5078dc8aab320a3a2819d3f7c7421994b
George Orlov <110418328+Tanjero19@users.noreply.github.com>
2023-12-04 02:08:58 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.menu');
 let body = document.querySelector('body');
+let headerLink = document.getElementsByClassName('header-link');
 
 menuBtn.addEventListener('click', function () {
     menuBtn.classList.toggle('active');
     menu.classList.toggle('active');
-})
-
-menuBtn.addEventListener('click', function () {
     body.classList.toggle('no_content');
-})
-
-menuBtn.addEventListener('click', function () {
     menuBtn.classList.toggle('btn-close');
 })
+
+function closeqwe() {
+    menuBtn.classList.remove('active');
+    menu.classList.remove('active');
+    body.classList.remove('no_content');
+    menuBtn.classList.remove('btn-close');
+}
